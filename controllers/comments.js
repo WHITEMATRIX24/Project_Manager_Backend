36adcb768cff5f23bc2193575b99e923efaa7aca
Jacob Louis <louisjacob8@gmail.com>
2025-04-07 11:51:40 +0530

    fix a small bug

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,10 +1,14 @@
 const Comment = require("../models/comment");
 
-export const getCommentsController = async (req, res) => {
+const getCommentsController = async (req, res) => {
   try {
     const comments = await Comment.find();
     res.json(comments);
   } catch (error) {
     res.status(500).json({ message: "server error", error });
   }
+};
+
+module.exports = {
+  getCommentsController,
 };
